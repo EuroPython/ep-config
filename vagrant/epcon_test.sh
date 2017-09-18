f7d6731ef1678fdc453b07a9686895077e64934e
Alexandre M. Savio <alexsavio@gmail.com>
2017-09-18 23:44:51 +0200

    epcontest vm: fix permissions for .ssh files

--- vagrant/epcon_test.sh
+++ vagrant/epcon_test.sh
@@ -9,7 +9,7 @@ cp -f /vagrant/ansible/roles/ssh/files/id_rsa.pub ~/.ssh/
 cp -f /vagrant/ansible/roles/ssh/files/id_rsa.pub ~/.ssh/authorized_keys
 cp -f /vagrant/ansible/roles/ssh/files/known_hosts ~/.ssh/known_hosts
 
-chown vagrant:vagrant ~/.ssh/id_rsa ~/.ssh/id_rsa.pub ~/.ssh/config ~/.ssh/known_hosts
+chown ubuntu:ubuntu ~/.ssh/id_rsa ~/.ssh/id_rsa.pub ~/.ssh/config ~/.ssh/known_hosts
 chmod 700 ~/.ssh
 chmod 644 ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/config ~/.ssh/id_rsa ~/.ssh/known_hosts
